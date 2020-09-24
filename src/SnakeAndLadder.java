1749f8d8f6195130897600a1807cc36004373365
ASHUTOSH <ashutosh@192.168.1.4>
2020-09-24 18:50:53 +0530

    [Ashutosh]Added:UC1 Push

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -4,6 +4,9 @@ public class SnakeAndLadder {
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Snake and Ladder Game");
+		System.out.println("");
+
+		int PlayerPosition1=0;
 
 		
 	}
